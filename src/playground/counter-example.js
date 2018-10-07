0e3fc8d04bb88d5ec35878179aaa95f732399cbc
simas-usas <simas.s.usas@gmail.com>
2018-10-07 22:57:28 +0300

    Webpack configuration

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -8,12 +8,26 @@ class Counter extends React.Component {
           count: 0
         };
     }
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);
+
+        if (!isNaN(count)) {
+          this.setState(() => ({ count }));
+        }
+    
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count)
+            localStorage.setItem('count', this.state.count);
+    }
     handleAddOne() {
         this.setState((prevState) => {
             return {
                 count: prevState.count + 1 
             };
-        });    }
+        });    
+    }
     handleMinusOne() {
         this.setState((prevState) => {
             return {
